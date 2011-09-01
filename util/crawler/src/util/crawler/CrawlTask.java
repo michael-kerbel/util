4e1912ccae28e718ba5df2835580d741329d67b2
Michael <75951847+michael-kerbel@users.noreply.github.com>
2011-09-01 14:15:50 +0200

    better retry logging

--- util/crawler/src/util/crawler/CrawlTask.java
+++ util/crawler/src/util/crawler/CrawlTask.java
@@ -199,7 +199,7 @@ public class CrawlTask implements Runnable {
          }
          catch ( Exception argh ) {
             if ( maxRetries > 0 ) {
-               _log.warn("Failed to get page" + (_params.isUseProxies() ? " using proxy " + proxy : "") + ": " + argh);
+               _log.info("Failed to get page" + (_params.isUseProxies() ? " using proxy " + proxy : "") + " (will retry): " + argh);
             } else {
                _log.warn("Failed to get page", argh);
                if ( argh instanceof UnexceptedStatuscodeException ) {
