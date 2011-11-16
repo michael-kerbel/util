fdb8c8fa081582672ed08efd5e5d7a202112dd80
Michael <75951847+michael-kerbel@users.noreply.github.com>
2011-11-16 12:37:53 +0100

    Sommerzeit fix

--- util/usagetracking/src/util/usagetracking/FlotHelper.java
+++ util/usagetracking/src/util/usagetracking/FlotHelper.java
@@ -3,35 +3,38 @@ package util.usagetracking;
 import gnu.trove.list.TLongList;
 
 import java.util.List;
-
-import util.time.TimeUtils;
+import java.util.TimeZone;
 
 
 public class FlotHelper {
 
-   public static long fix( long t ) {
-      return t + 2 * TimeUtils.HOUR_IN_MILLIS;
+   public static long offset( long t ) {
+      return TimeZone.getDefault().getOffset(t);
    }
 
    public static String toString( TLongList keys, List<int[]> data, TrackingId id ) {
+      long timeOffset = offset(keys.size() == 0 ? 0 : keys.get(0));
       final StringBuilder s = new StringBuilder("[");
       for ( int i = 0, length = keys.size(); i < length; i++ ) {
          long t = keys.get(i);
-         s.append(s.length() > 1 ? "," : "").append("[").append(fix(t)).append(",").append(data.get(i)[id.getId()]).append("]");
+         s.append(s.length() > 1 ? "," : "").append("[").append(t + timeOffset).append(",").append(data.get(i)[id.getId()]).append("]");
       }
 
       return s.append("]").toString();
    }
 
    public static String toStringAvg( TLongList keys, List<int[]> data, TrackingId sum, TrackingId n ) {
+      long timeOffset = offset(keys.size() == 0 ? 0 : keys.get(0));
       final StringBuilder s = new StringBuilder("[");
       for ( int i = 0, length = keys.size(); i < length; i++ ) {
          long t = keys.get(i);
          int a = data.get(i)[sum.getId()];
          int b = data.get(i)[n.getId()];
          int c = Math.round(a / (float)b);
-         if ( b == 0 ) c = 0;
-         s.append(s.length() > 1 ? "," : "").append("[").append(fix(t)).append(",").append(c).append("]");
+         if ( b == 0 ) {
+            c = 0;
+         }
+         s.append(s.length() > 1 ? "," : "").append("[").append(t + timeOffset).append(",").append(c).append("]");
       }
 
       return s.append("]").toString();
