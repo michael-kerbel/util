e4e2d937e2c5692b6215c9e047f84beaade2b5cf
Michael <75951847+michael-kerbel@users.noreply.github.com>
2013-07-25 17:59:17 +0200

    added ExecutorUtils

---
+++ util/base/src/util/concurrent/ExecutorUtils.java
@@ -0,0 +1,85 @@
+package util.concurrent;
+
+import gnu.trove.map.TIntIntMap;
+import gnu.trove.map.hash.TIntIntHashMap;
+
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.Nonnull;
+
+import org.apache.log4j.Logger;
+
+import util.time.TimeUtils;
+
+
+public class ExecutorUtils {
+
+   private static Logger _log = Logger.getLogger(ExecutorUtils.class);
+
+
+   /**
+    * Waits for all scheduled tasks of an Executor to finish, and shuts it down.<br/>
+    * Do not use this, when adding tasks to the Executor after calling this method!<br/><br/>
+    * Uses 1000 ms as polling interval to check if finished and a max time of 30000 ms 
+    * for awaiting termination of the Executor after all tasks have been completed.
+    */
+   public static void awaitCompletion( @Nonnull ThreadPoolExecutor executor ) {
+      awaitCompletion(executor, 1000, 30000);
+   }
+
+   /**
+    * Waits for all scheduled tasks of an Executor to finish, and shuts it down.<br/>
+    * Do not use this, when adding tasks to the Executor after calling this method!  
+    * @param sleepIntervalInMillis The polling interval for the checks on the Executor
+    * @param shutdownWaitIntervalInMillis The maximum time to wait for the shutdown of the Executor after all tasks have been completed
+    */
+   public static void awaitCompletion( @Nonnull ThreadPoolExecutor executor, long sleepIntervalInMillis, long shutdownWaitIntervalInMillis ) {
+      long taskCount = getTaskCount(executor);
+      while ( true ) {
+         boolean check = false;
+         synchronized ( executor ) {
+            if ( executor.isShutdown() ) {
+               throw new RuntimeException("Task executor was stopped early.");
+            }
+            check = executor.getQueue().size() > 0;
+            check |= executor.getActiveCount() > 0;
+         }
+         if ( !check ) {
+            break;
+         }
+         TimeUtils.sleepQuietly(sleepIntervalInMillis);
+      }
+
+      synchronized ( executor ) {
+         executor.shutdown();
+         try {
+            executor.awaitTermination(shutdownWaitIntervalInMillis, TimeUnit.MILLISECONDS);
+         }
+         catch ( InterruptedException argh ) {
+            // ignore
+         }
+      }
+
+      long completedTaskCount = executor.getCompletedTaskCount();
+      if ( taskCount > completedTaskCount ) {
+         _log.warn("executed too few tasks! At least " + taskCount + " were scheduled, but only " + completedTaskCount + " were executed.");
+      }
+   }
+
+   private static long getTaskCount( ThreadPoolExecutor executor ) {
+      TIntIntMap counts = new TIntIntHashMap();
+      for ( int i = 5; i >= 0; i-- ) {
+         counts.adjustOrPutValue((int)executor.getTaskCount(), 1, 1);
+      }
+      int maxCount = 0, taskCount = 0;
+      for ( int tc : counts.keys() ) {
+         int c = counts.get(tc);
+         if ( c > maxCount ) {
+            maxCount = c;
+            taskCount = tc;
+         }
+      }
+      return taskCount;
+   }
+}
