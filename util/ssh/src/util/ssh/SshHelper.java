8af729573d77f41778231a02036510527b6cc163
Michael <75951847+michael-kerbel@users.noreply.github.com>
2016-03-09 21:00:07 +0100

    - switch to commons-lang3

--- util/ssh/src/util/ssh/SshHelper.java
+++ util/ssh/src/util/ssh/SshHelper.java
@@ -17,7 +17,7 @@ import java.util.Properties;
 import java.util.regex.Pattern;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
